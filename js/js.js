ffc966be2e43ea01421fd284a4705f3c1365b69e
Olesia Davniuk <lv250385sov@gmail.com>
2018-02-19 22:29:09 +0200

    refreshed portfolio by sass

--- js/js.js
+++ js/js.js
@@ -6,35 +6,6 @@ document.body.onload = function() {
 		}
 	}, 2000);
 }
-
-$(document).ready(function(){
-   var destination = $(window).height();
-   	  
-   $("#glyphicon").effect( "pulsate", {
-                  times: 100,
-                  distance: 100
-    });
-
-   $('#glyphicon').click(function() {
-   		$('html, body').animate({scrollTop: destination}, 1800);
-        
-         $('#glyphicon').css('visibility', 'hidden');
-   });
-
-   $(window).scroll(function() {
-
-      if($(window).scrollTop() === 0) {
-         $('#glyphicon').css('visibility', 'visible');
-      }
-
-   });
-
-   $('.contact').click(function() {
-      window.location = 'https://mail.google.com';
-   });
-});
-
-   
  
 
 
